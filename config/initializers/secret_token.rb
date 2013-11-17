d6a7108f546c1c0a373ea90b7473a70021078848
Firehose <firehose@firehose-VirtualBox.(none)>
2013-11-17 14:08:28 -0500

    Mapper setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapper::Application.config.secret_key_base = '19f7e9ade82f27d11dea41b48470220ad8310ae82ffb2c260707541252a6d2565c997b7c92214f9483ec10aecb011cb7ae3d68165d1b348b6671bcea418fd7a3'
