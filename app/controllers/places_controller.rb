3077a347e949421f2d37cc13f1d622f3d1ebb711
Firehose <firehose@firehose-VirtualBox.(none)>
2013-11-17 15:31:17 -0500

    Add Form for the Place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,5 +4,29 @@ def index
 	@place = Place.last
 end
 
+def new
+
+@place=Place.last
+#@place=Place.new
+
+end
+
+def update
+
+#Place.create(:title => "  ", :address => "    ")
+
+@place = Place.create( place_params )
+
+redirect_to root_path
+
+end
+
+private
+
+def place_params
+
+params.require(:place).permit(:title, :address)
+
+end
 
 end
